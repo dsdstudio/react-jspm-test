60874c35a25a59a271bd4437c0c4705c2af1cdb9
보롱킴 <dsdgun@gmail.com>
2015-12-01 13:19:59 +0900

    history 쪽에서 script 오류 나는 부분 수정

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ import {
 import { Route, Router, IndexRoute, Link, History } from 'react-router';
 import { Provider, connect } from 'react-redux';
 import createLogger from 'redux-logger';
-import { createHashHistory } from 'history';
+import createHistory from 'history/lib/createHashHistory';
 
 class App extends Component {
 	render() {
@@ -19,17 +19,37 @@ class App extends Component {
 	}
 }
 
-const reducer = combineReducers({
-})
+class StartPage extends Component {
+	render() {
+		return (
+			<div>
+				<h2>jspm + react + react-redux, redux-router scaffolding</h2>
+				<p>
+					기본구조만 잡아두었습니다.
+				</p>
+			</div>
+		);
+	}
+}
+StartPage.PropTypes = {
+	children: PropTypes.node
+};
+
+const reducer = combineReducers({ 
+	router:routerStateReducer
+});
+
 const store = compose(
 	applyMiddleware(thunkMiddleware, createLogger()),
-	reduxReactRouter({ createHashHistory })
+	reduxReactRouter({ createHistory })
 )(createStore)(reducer);
 
 ReactDOM.render(
 	<Provider store={store}>
 		<ReduxRouter>
-			<Route path="/" component={App}></Route>
+			<Route path="/" component={App}>
+				<IndexRoute component={StartPage} />
+			</Route>
 		</ReduxRouter>
 	</Provider>
 , document.getElementById('app'));
